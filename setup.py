46ca7da6030433f4346b31b7c420ff127e8c6966
ludlows <ludlows@foxmail.com>
2019-12-28 19:37:45 -0500

    initial setup.py for the whole package

---
+++ setup.py
@@ -0,0 +1,38 @@
+# 2019-12
+# github.com/ludlows
+# Lite Python Package for Room Impulse Response
+import numpy
+from setuptools import find_packages
+from distutils.core import setup
+from distutils.extension import Extension
+
+from Cython.Build import cythonize, build_ext
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+extensions = [
+    Extension(
+        "cyrir",
+        ["pyrir/cyrir/cyrir.pyx"],
+        include_dirs=[numpy.get_include()],
+        language="c")
+]
+setup(
+    name="pyrir",
+    version="0.0.0",
+    author="ludlows",
+    description="Light Python Package for Room Impulse Response",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ludlows/pyrir",
+    packages=find_packages(),
+    package_data={'pyrir':['cyrir/cyrir.pyx','cyrir/rir.c']},
+    ext_modules=cythonize(extensions),
+    setup_requires=['numpy', 'scipy', 'cython'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
