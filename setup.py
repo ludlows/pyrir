22c200aa397b238ea230c3e7fef4bcb0da9b5f57
ludlows <ludlows@foxmail.com>
2019-12-28 20:51:53 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ import numpy
 from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
-
-from Cython.Build import cythonize, build_ext
+from Cython.Build import build_ext,cythonize
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 extensions = [
     Extension(
-        "cyrir",
+        "pyrir.cyrir.cyrir",
         ["pyrir/cyrir/cyrir.pyx"],
         include_dirs=[numpy.get_include()],
         language="c")
@@ -27,7 +26,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ludlows/pyrir",
     packages=find_packages(),
-    package_data={'pyrir':['cyrir/cyrir.pyx','cyrir/rir.c']},
+    cmdclass = {'build_ext': build_ext},
     ext_modules=cythonize(extensions),
     setup_requires=['numpy', 'scipy', 'cython'],
     classifiers=[
